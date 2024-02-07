b576691ddc204c3fd6544c871e8526391857fafa
fruit-kit <kotrutsa666@gmail.com>
2024-02-07 19:15:36 +0200

    Initial commit

---
+++ Race/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Race
+//
+//  Created by Robert Kotrutsa on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
