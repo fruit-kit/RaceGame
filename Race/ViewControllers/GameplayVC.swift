608a23f94727e52c7ffcce2b129ce4ef76058fbf
fruit-kit <kotrutsa666@gmail.com>
2024-02-16 14:22:54 +0200

    Add UIImageView objects for game assets

--- Race/ViewControllers/GameplayVC.swift
+++ Race/ViewControllers/GameplayVC.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class GameplayVC: UIViewController {
     
-    
+    var car = UIImageView(image: UIImage(named: "car"))
+    var rock = UIImageView(image: UIImage(named: "rock"))
+    var tree = UIImageView(image: UIImage(named: "tree"))
+    var barrier = UIImageView(image: UIImage(named: "barrier"))
 
     override func viewDidLoad() {
         super.viewDidLoad()
