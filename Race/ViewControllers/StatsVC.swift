3b769358a31c13bee3b3dc2c87c6a77a2517e0fe
fruit-kit <kotrutsa666@gmail.com>
2024-03-13 06:57:26 +0200

    Remove setupBlurEffectToBackground method from StatsVC

--- Race/ViewControllers/StatsVC.swift
+++ Race/ViewControllers/StatsVC.swift
@@ -9,26 +9,12 @@ import UIKit
 
 class StatsVC: UIViewController {
     
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         
-        setupBlurEffectToBackground()
     }
     
-    private func setupBlurEffectToBackground() {
-        
-        let blureEffect = UIBlurEffect(style: .light)
-        
-        let bluredEffectView = UIVisualEffectView(effect: blureEffect)
-        
-        bluredEffectView.frame = self.view.bounds
-        
-        bluredEffectView.alpha = 1
-        
-        bluredEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        self.view.addSubview(bluredEffectView)
-    }
+    
     
 }
